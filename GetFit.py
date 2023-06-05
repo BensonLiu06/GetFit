39aaec6a599c2b3d930920bf552a781b410a3941
stanleyly06 <130597539+stanleyly06@users.noreply.github.com>
2023-06-04 21:16:42 -0400

    Update and rename usernamepassword.py to GetFit.py

---
+++ GetFit.py
@@ -0,0 +1,74 @@
+from tkinter import *
+import mysql.connector
+from mysql.connector import errorcode
+from GetFitDB import *
+from LoginWindow import *
+from RegisterUserWindow import *
+from AppWindow import *
+from PopupBox import *
+from CheckUserNameExists import *
+from PasswordHash import *
+
+# Implementation of main window
+
+def mainAppWindow():
+    #global mainWindow
+    # Create an instance of tkinter frame or window
+    mainWindow = Tk()
+
+    # Set the width and height of the main window
+    w = 600 # Width 
+    h = 500 # Height
+ 
+    # Determine the size of the screen
+    screen_width =  mainWindow.winfo_screenwidth()  # Width of the screen
+    screen_height = mainWindow.winfo_screenheight() # Height of the screen
+ 
+    # Calculate starting x and y coordinates to center the main window on the screen
+    x = (screen_width / 2) - (w / 2)
+    y = (screen_height / 2) - (h / 2)
+ 
+    mainWindow.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    mainWindow.resizable(True, True)
+    mainWindow.title("GetFit")
+
+    # Create three frames in the window
+
+    # Create a frame for the main app window
+    appWindow = Frame(mainWindow)
+    appWindow.grid(sticky='nsew')
+    appWindow.grid_forget()
+
+    # Create a frame for the user registration window
+    userRegistrationWindow = Frame(mainWindow)
+    userRegistrationWindow.grid(sticky='nsew')
+    userRegistrationWindow.grid_forget()
+
+    # Create a frame for the user login window
+    userLoginWindow = Frame(mainWindow)
+    userLoginWindow.grid(sticky='nsew')
+
+    # Setup parameter to access the mySQL server
+    dbHostname = "localhost"
+    dbUsername = "root"
+    dbPort = "3306"
+    dbPassword = ""
+    dbName = "GetFitDB"
+
+    # Connect to the GetFit database
+    accessDatabase(mainWindow, dbHostname, dbUsername, dbPort, dbPassword, dbName)
+    
+    dbConnection = mysql.connector.connect(
+        host = dbHostname,
+        user = dbUsername,
+        port = dbPort,
+        password = dbPassword,
+        database = dbName
+    )
+    dbCursor = dbConnection.cursor()
+
+    createLoginWindow(mainWindow, userLoginWindow, userRegistrationWindow, appWindow, dbConnection, dbCursor)
+
+    mainWindow.mainloop()
+
+mainAppWindow()
