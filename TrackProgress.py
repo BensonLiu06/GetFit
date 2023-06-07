54635ec421178ea321a58d0522881f7149408036
stanleyly06 <130597539+stanleyly06@users.noreply.github.com>
2023-06-07 14:47:22 -0400

    Create TrackProgress.py

---
+++ TrackProgress.py
@@ -0,0 +1,71 @@
+from PopupBox import *
+# Implementation of Track Progress window
+def createTrackProgressWindow(mainWindow, parentWindow, dbConnection, dbCursor, username):
+    # Hide the App window
+    parentWindow.grid_forget()
+
+    # Create a frame for the Track Progress window
+    trackProgressWindow = ttk.Frame(mainWindow, padding=(3,3,12,12))
+    trackProgressWindow.grid(sticky = (N, S, E, W))
+
+    # Setup the User Registration window
+    trackProgressWindow.columnconfigure(0, weight = 1)
+    trackProgressWindow.columnconfigure(1, weight = 1)
+    trackProgressWindow.rowconfigure(0, weight = 1)
+    trackProgressWindow.rowconfigure(1, weight = 1)
+
+    # Create all the main frame containers
+    topFrame = ttk.Frame(trackProgressWindow, width = 600, height = 50, relief = 'groove', borderwidth = 2)
+    bottomFrame = ttk.Frame(trackProgressWindow, width = 300, height = 700, relief = 'groove', borderwidth = 2)
+    buttonFrame = ttk.Frame(trackProgressWindow, width = 300, height = 50, relief = 'groove', borderwidth = 2)
+
+    # Layout all of the main frame containers
+    topFrame.grid(column = 0, row = 0, columnspan = 2, rowspan = 1, padx = 5, pady = 5, sticky=(N, S, E, W))
+    bottomFrame.grid(column = 0, row = 1, columnspan = 2, rowspan = 11, padx = 5, pady = 5, sticky=(N, S, E, W))
+    buttonFrame.grid(column = 0, row = 12, columnspan = 2, rowspan = 1, padx = 5, pady = 5, sticky=(N, S, E, W))
+
+    topFrame.columnconfigure(0, weight = 1)
+    topFrame.columnconfigure(0, weight = 1)
+    bottomFrame.columnconfigure(0, weight = 1)
+    bottomFrame.columnconfigure(0, weight = 1)
+    buttonFrame.columnconfigure(0, weight = 1)
+    buttonFrame.columnconfigure(0, weight = 1)
+
+    topFrame.rowconfigure(0, weight = 0)
+    bottomFrame.rowconfigure(1, weight = 1)
+    bottomFrame.rowconfigure(2, weight = 1)
+    bottomFrame.rowconfigure(3, weight = 1)
+    bottomFrame.rowconfigure(4, weight = 1)
+    bottomFrame.rowconfigure(5, weight = 1)
+    bottomFrame.rowconfigure(6, weight = 1)
+    bottomFrame.rowconfigure(7, weight = 1)
+    bottomFrame.rowconfigure(8, weight = 1)
+    bottomFrame.rowconfigure(9, weight = 1)
+    bottomFrame.rowconfigure(10, weight = 1)
+    bottomFrame.rowconfigure(11, weight = 1)
+    buttonFrame.rowconfigure(12, weight = 1)
+
+    # Create an information label 
+    informationLabel =ttk.Label(topFrame, text = "Track your progress")
+    informationLabel.grid(column = 0, row = 0, columnspan = 2)
+
+    # Create an information label 
+    informationLabel2 =ttk.Label(bottomFrame, text = "Danger - Construction Area - Keep Out")
+    informationLabel2.grid(column = 0, row = 1, columnspan = 2)
+
+
+    popupBox(mainWindow, trackProgressWindow, "Warning", "Track Progress Window implementation incomplete")
+    
+    # Create button widget to Cancel the Update Profile & Settings window
+    cancelButton = ttk.Button(buttonFrame, text = "Cancel", command = lambda : 
+                                   cancelTrackProgressWindow(mainWindow, trackProgressWindow, parentWindow))
+    cancelButton.grid(column = 1, row = 12, pady = 5)
+
+# Implementation of the Cancel button for the Track Progress window
+# Closes the Track Progress window and returns to the App window
+def cancelTrackProgressWindow(mainWindow, parentWindow, appWindow):
+    # Hide the Track Progress window
+    parentWindow.grid_forget()
+
+    # Show the App window
+    appWindow.grid(sticky = (N, S, E, W))
