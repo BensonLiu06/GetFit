babdcd5ab1245f88fe11347a97e7822d944d6fa8
mahirzaman2510 <129217554+mahirzaman2510@users.noreply.github.com>
2023-06-11 22:01:06 -0400

    Update getfitdb.py

--- getfitdb.py
+++ getfitdb.py
@@ -1,3 +1,8 @@
+from tkinter import *
+import mysql.connector
+from mysql.connector import errorcode
+from PopupBox import *
+
 def accessDatabase(mainWindow, dbHostname, dbUsername, dbPort, dbPassword, dbName):
     # Call the function to check if the database exists
     dbExists = checkDatabaseExists(mainWindow, dbHostname, dbUsername, dbPort, dbPassword, dbName)
@@ -33,3 +38,57 @@ def accessDatabase(mainWindow, dbHostname, dbUsername, dbPort, dbPassword, dbNam
         except mysql.connector.Error as error:
             errorMessage = "Error Code: " + str(error.errno) + "\n" + "SQLSTATE: " + error.sqlstate + "\n" + "Message: " + error.msg
             popupBox(mainWindow, mainWindow, "Error", errorMessage)
+    
+    # Call the function to check if the database exists
+    dbExists = checkDatabaseExists(mainWindow, dbHostname, dbUsername, dbPort, dbPassword, dbName)
+
+    if dbExists:
+        pass
+    else:
+        # Create the GetFit database if it does not exist
+        dbConnection = mysql.connector.connect(
+            host = dbHostname,
+            user = dbUsername,
+            port = dbPort,
+            password = dbPassword,
+        )
+        dbCursor = dbConnection.cursor()
+
+        try:
+            dbCursor.execute(f"CREATE DATABASE {dbName}")
+
+            dbConnection = mysql.connector.connect(
+                host = dbHostname,
+                user = dbUsername,
+                port = dbPort,
+                password = dbPassword,
+                database = dbName
+            )
+            dbCursor = dbConnection.cursor()
+            dbCursor.execute("CREATE TABLE User (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), password_hash CHAR(60), security_question VARCHAR(256), security_response VARCHAR(64))")
+            dbCursor.execute("CREATE TABLE UserInfo (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), fullname VARCHAR(256), birthdate DATE DEFAULT(CURRENT_DATE), phone_number VARCHAR(50), email VARCHAR(256), gender VARCHAR(10), height DECIMAL(4,1) DEFAULT 0, weight DECIMAL (4,1) DEFAULT 0, image_path VARCHAR(256))")
+            dbConnection.commit()
+            dbConnection.close()
+        except mysql.connector.Error as error:
+            errorMessage = "Error Code: " + str(error.errno) + "\n" + "SQLSTATE: " + error.sqlstate + "\n" + "Message: " + error.msg  
+            popupBox(mainWindow, mainWindow, "Error", errorMessage)
+
+
+def checkDatabaseExists(mainWindow, dbHostname, dbUsername, dbPort, dbPassword, dbName):
+    try:
+        dbConnection = mysql.connector.connect(
+            host = dbHostname,
+            user = dbUsername,
+            port = dbPort,
+            password = dbPassword,
+            database = dbName
+        )
+        dbConnection.close()
+        return True
+    except mysql.connector.Error as error:
+        if error.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+            return False
+        else:
+            errorMessage = "Error Code: " + str(error.errno) + "\n" + "SQLSTATE: " + error.sqlstate + "\n" + "Message: " + error.msg
+            popupBox(mainWindow, mainWindow, "Error", errorMessage)
+            raise
