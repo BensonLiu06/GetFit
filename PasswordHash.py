dd02ff98a4b12fb1a4193cf2ffa7e4347ab54611
stanleyly06 <130597539+stanleyly06@users.noreply.github.com>
2023-06-12 11:32:34 -0400

    Update PasswordHash.py

--- PasswordHash.py
+++ PasswordHash.py
@@ -2,10 +2,16 @@ import bcrypt
 
 # Create a password hash
 def createPasswordHash(password):
+    # Encode the password
     passwordBytes = password.encode('utf8')
 
+    # Specify the salt
     salt = bcrypt.gensalt(14)
+
+    # Encrypt the password using the bcrypt algorithm with the provided salt
     passwordHashBytes = bcrypt.hashpw(passwordBytes, salt)
+
+    # Decode the hashed password
     passwordHashString = passwordHashBytes.decode()
 
     return passwordHashString
